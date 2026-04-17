edf2bd03d14f8e03e2df2f40a8da2eea00bf746e
blenda <blendaarama@gmail.com>
2026-04-17 21:16:31 +0200

    h

--- src/main/java/com/example/flower_shop/controller/BouquetFlowerController.java
+++ src/main/java/com/example/flower_shop/controller/BouquetFlowerController.java
@@ -4,7 +4,6 @@ import com.example.flower_shop.model.BouquetFlower;
 import com.example.flower_shop.repository.BouquetFlowerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
