09353d110423ff0ac6174d7ed088b83fe1959feb
rt72947 <rt72947@ubt-uni.net>
2026-04-20 21:37:04 +0200

    Update frontend port from 5173 to 3000"

--- src/main/java/com/example/flower_shop/controller/CustomerController.java
+++ src/main/java/com/example/flower_shop/controller/CustomerController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/customers")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CustomerController {
    
     private final CustomerRepository customerRepository;
